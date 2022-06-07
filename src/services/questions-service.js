ff3797f85ff8e1ee2d5ddc6088fc9f2fe1df8004
Iris Wang <wang.ir@northeastern.edu>
2022-06-07 01:19:30 -0400

    server fix

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.ANIME_API_BASE || "http://localhost:4000/api"
+const API_BASE = process.env.ANIME_API_BASE;
 const QUESTION_API = `${API_BASE}/questions`
 
 export const fetchQuestionByID = async (questionID) => {
