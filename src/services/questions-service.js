1f4a7edb542558b01daad42156ce05525b464f0e
Iris Wang <wang.ir@northeastern.edu>
2022-06-07 01:30:23 -0400

    made local dev easier

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_ANIME_API;
+const API_BASE = process.env.REACT_APP_ANIME_API || "http://localhost:4000/api";
 const QUESTION_API = `${API_BASE}/questions`
 
 export const fetchQuestionByID = async (questionID) => {
